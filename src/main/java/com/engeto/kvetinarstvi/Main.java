ea677e0e4c597e1afe75e0e16d3c5250db1f7785
Petr Liszok <liszok@email.cz>
2023-04-30 14:26:38 +0200

    commit

--- src/main/java/com/engeto/kvetinarstvi/Main.java
+++ src/main/java/com/engeto/kvetinarstvi/Main.java
@@ -1,5 +1,32 @@
-package com.engeto.kvetinarstvi;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package com.engeto.kvetinarstvi;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/kvetinarstvi","zahradnik","Heslo123");
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("select*from flowers");
+        while (resultSet.next())
+            System.out.println(resultSet.getString("name"));
+
+//        statement.executeUpdate("insert into flowers (name) values ('Bledule')");
+//        statement.executeUpdate("insert into flowers (name) values ('Kopretina')");
+
+//        statement.executeUpdate("update flowers set poisonous=0");
+//
+//        "sloupec pooisonous jsem měl jako varchar, aby šel změnit na boolean, musel jsem tam nejdříve nastavit hodnotu.
+//        Implicitně tam bylo null a to házelo chybu."
+
+//        statement.executeUpdate("update flowers set description='Pozor na cibulku - obsahuje největší koncentraci jedu!' where name='Bledule'");
+
+//        statement.executeUpdate("update flowers set colour='bílá' where name='Bledule'");
+
+//        statement.executeUpdate("update flowers set colour='bílá', description='z čeledi hvězdnicovitých' where name='Kopretina'");
+
+//        statement.executeUpdate("update flowers set poisonous='1' where name='Bledule'");
+
+        statement.executeUpdate("delete from flowers where poisonous='1'");
     }
 }
